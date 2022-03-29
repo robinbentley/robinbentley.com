22f0e99ae1fdc27314596cc3ca63b5cc72eff475
Robin Bentley <robinbentley@me.com>
2022-03-29 10:48:23 +0100

    Package updates

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,48 +1,56 @@
 // utilities
-import gulp from 'gulp'
-import del from 'del'
+import gulp from "gulp";
 
 // styling
-import sass from 'gulp-sass'
-import cssnano from 'gulp-cssnano'
+import sass from "gulp-sass";
+import cssnano from "gulp-cssnano";
 
 // images
-import imagemin from 'gulp-imagemin'
+import imagemin from "gulp-imagemin";
 
 // html
-import htmlmin from 'gulp-htmlmin'
+import htmlmin from "gulp-htmlmin";
 
 // compile sass and process with cssnano
-gulp.task('styles', () => {
-  gulp.src('./source/sass/site.sass')
-    .pipe(sass({
-      indentedSyntax: true,
-      includePaths: ['node_modules']
-    }))
-    .pipe(cssnano({
-      autoprefixer: {browsers: ['last 2 versions'], add: true},
-      discardComments: {removeAll: true}
-    }))
-    .pipe(gulp.dest('./static/css/'))
-})
+gulp.task("styles", () => {
+  gulp
+    .src("./source/sass/site.sass")
+    .pipe(
+      sass({
+        indentedSyntax: true,
+        includePaths: ["node_modules"],
+      })
+    )
+    .pipe(
+      cssnano({
+        autoprefixer: { browsers: ["last 2 versions"], add: true },
+        discardComments: { removeAll: true },
+      })
+    )
+    .pipe(gulp.dest("./static/css/"));
+});
 
 // optimize image assets
-gulp.task('images', () => {
-  gulp.src('./source/images/**/*.{png,jpg,jpeg,gif,svg}')
+gulp.task("images", () => {
+  gulp
+    .src("./source/images/**/*.{png,jpg,jpeg,gif,svg}")
     .pipe(imagemin())
-    .pipe(gulp.dest('./static/images/'))
-})
+    .pipe(gulp.dest("./static/images/"));
+});
 
 // minify html files (hugo doesn't currently do this by default)
-gulp.task('htmlmin', () => {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(gulp.dest('./public/'))
-})
+gulp.task("htmlmin", () => {
+  return gulp
+    .src("./public/**/*.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest("./public/"));
+});
 
 // default task to watch and compile assets
-gulp.task('default', () => {
-  gulp.watch('./source/sass/**/*.{sass,scss,css}', gulp.series('styles'));
-})
+gulp.task("default", () => {
+  gulp.watch("./source/sass/**/*.{sass,scss,css}", gulp.series("styles"));
+});
